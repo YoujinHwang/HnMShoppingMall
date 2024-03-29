e26d8595287ae04968e6a952f717b66cb71a569e
Youjin Hwang <rgh747052@gmail.com>
2024-03-30 00:20:09 +0900

    update

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,24 +3,50 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
 const Navbar = ({authenticate,setAuthenticate}) => {
     const menuList=['여성','Divided',"남성","신생아/유아","H&M Home","Sale","지속가능성",];
-    const navigate=useNavigate()
+    const navigate=useNavigate();
+    const [width, setWidth]=useState(0);
     
     const goToLogin=()=>{
         navigate("/login");
     }
+    const search=(event)=>{
+        if(event.key==="Enter") {
+            //입력한 검색어를 읽어와서
+            let keyword=event.target.value;
+            
+            //url을 바꿔준다
+            navigate(`/?q=${keyword}`)
+        }
+    }
   
   return (
   <div>
+    <button onClick={() => setWidth("250px")}>Category</button>
+
+    <div className="side-menu" style={{ width: width }}>
+        <button className="closebtn" onClick={() => setWidth(0)}>
+          &times;
+        </button>
+        <div className="side-menu-list" id="menu-list">
+          {menuList.map((menu, index) => (
+            <li key={index}>{menu}</li>
+          ))}
+        </div>
+    </div>
+
     <div>
         {authenticate ? (
-            <div class="logout-button" onClick={()=>setAuthenticate(false)}>
+            <div className="logout-button" onClick={()=>setAuthenticate(false)}>
             <FontAwesomeIcon icon={faUser}/>
             <div>로그아웃</div>
         </div>
         ):(
-        <div class="login-button" onClick={goToLogin}>
+        <div className="login-button" onClick={goToLogin}>
             <FontAwesomeIcon icon={faUser}/>
             <div>로그인</div>
         </div>
@@ -28,21 +54,23 @@ const Navbar = ({authenticate,setAuthenticate}) => {
         
     </div>
     <div className="nav-section">
+        <Link to="/">
         <img alt=""
             width={100}
             src="https://thumbs.dreamstime.com/z/hm-h-m-logo-editorial-illustrative-white-background-icon-vector-logos-icons-set-social-media-flat-banner-vectors-svg-eps-jpg-210442760.jpg"/>
+        </Link>
     </div>
     <div className="menu-area">
         
         <ul className="menu-list">
-            {menuList.map((menu)=>(
-                <li>{menu}</li>
+            {menuList.map((menu,index)=>(
+                <li key={index}>{menu}</li>
             ))}
         </ul>
         
         <div className="search-box">
             <FontAwesomeIcon icon={faSearch}/>
-            <input type="text"/>
+            <input type="text" onKeyPress={(event)=> search(event)}/>
         </div>
     </div>
 
@@ -50,4 +78,4 @@ const Navbar = ({authenticate,setAuthenticate}) => {
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -2,24 +2,27 @@ import React from 'react'
 import { useEffect,useState } from 'react'
 import ProductCard from '../component/ProductCard';
 import { Container,Row,Col } from 'react-bootstrap';
-
+import { useSearchParams } from 'react-router-dom';
 const ProductAll = () => {
   let [productList, setProductList]=useState([]);
+  const [query, setQuery]=useSearchParams();
 
   const getProducts=async ()=>{
-    let url=`https://my-json-server.typicode.com/YoujinHwang/HnMShoppingMall/products`;
+    let searchQuery=query.get('q')||""; //null이 추가되어야 하는 이유는?
+    console.log("쿼리값은?", searchQuery);
+    let url=`https://my-json-server.typicode.com/YoujinHwang/HnMShoppingMall/products?q=${searchQuery}`;
     let response=await fetch(url);
     let data=await response.json();
     setProductList(data);
   }
   useEffect(()=>{
     getProducts()
-  },[])
+  },[query]);
   return (
       <Container>
         <Row>
           {productList.map((menu)=>(
-            <Col lg={3} sm={12}>
+            <Col lg={3} sm={12} key={menu.id}>
               <ProductCard item={menu}/></Col>
           ))}
          
@@ -30,4 +33,4 @@ const ProductAll = () => {
   )
 }
 
-export default ProductAll
+export default ProductAll;
